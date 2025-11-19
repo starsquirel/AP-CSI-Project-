2cd55de828f0d0e5707615581a302c3b57724228
starsquirel <122704498+starsquirel@users.noreply.github.com>
2025-11-19 14:51:17 -0700

    Update main.py
    This is the version with persistent chips

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import random
 import time
+import json
+
 #these are the lists to check what type the role is
 Black = [15, 4, 2, 17, 6, 13, 11, 8, 10, 24, 33, 20, 31, 22, 29, 28, 35, 26]
 Red = [32, 19, 21, 25, 34, 27, 36, 30, 23, 5, 16, 1, 14, 9, 18, 7, 12, 3]
@@ -7,8 +9,13 @@ Low = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
 High = [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36]
 Even = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36]
 Odd = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35]
-weel = [0, 28, 9, 26, 30, 11, 7, 20, 32, 17, 5, 23, 34, 15, 3, 24, 36, 13, 1, 00, 27, 10, 25, 29, 12, 8, 19, 31, 18, 6, 21, 33, 16, 4, 23, 35, 14, 2]
+weel = [
+    0, 28, 9, 26, 30, 11, 7, 20, 32, 17, 5, 23, 34, 15, 3, 24, 36, 13, 1, 00,
+    27, 10, 25, 29, 12, 8, 19, 31, 18, 6, 21, 33, 16, 4, 23, 35, 14, 2
+]
 
+Chips = 100
+Wait = 0.01
 
 
 def Text(words):
@@ -21,83 +28,111 @@ def Text(words):
         print(words[index], end='', flush=True)
         index += 1
 
+
 def rouletteweel():
-# Chooses a random eliment in the weel
+    # Chooses a random eliment in the weel
     weel_out = random.choice(weel)
     return weel_out
-Text("hello this is roulet\n")  
+
+
+def StartUp():
+    Text("hello this is roulett\n")
+
+    try:
+        with open('mydata.json', 'r') as f:
+            Chips = json.load(f)
+    except FileNotFoundError:
+        Chips = 0
+
+    main(Chips)
+
+
+def Shutdown():
+    Text("goodbye\n")
+    time.sleep(1)
+    Text("saving\n")
+    with open('save.json', 'w') as f:
+        json.dump(Chips, f)
+    Text("shutting down\n")
+    exit()
+
+
 def main(Chips):
     while True:
-        
-        print("(1)to play,(2)to buy more chips,(3) exit\n")
+        with open('mydata.json', 'w') as f:
+            json.dump(Chips, f)
+        print(f"You have {Chips} chips\n\n(1)to play,(2)to buy more chips,(3) exit\n")
         Choice1 = input("what do you want to do: ")
         if Choice1 == "1":
-            Text ("(1)black,(2)red,(3)high,(4)low, (5)even, (6)odd\n")
-            Choice2 = input("what do you want to bet on: ")
+            Text(
+                "\n(1)black,(2)red,(3)high,(4)low, (5)even, (6)odd\n"
+            )
+            Choice2 = input("\nwhat do you want to bet on: ")
             print(Chips)
-            Bet = int(input("how much do you want to bet(whole num): "))
+            Bet = int(input("\nhow much do you want to bet(whole num): "))
             if Bet <= Chips and Bet > 0:
-                role = rouletteweel() 
-                print(f"You roled \n--{role}--\n")
+                role = rouletteweel()
+
+                if role in Black:
+                    print(f"\n{role} black\n")
+                if role in Red:
+                    print(f"\n{role} red\n")
                 if Choice2 == "1":
                     if role in Black:
                         Text("you win\n")
                         Chips = Chips + Bet
-                        print(f"you win! you now have:{Chips} chips\n") 
-                    else: 
+                        print(f"you now have:{Chips} chips\n")
+                    else:
                         Chips = Chips - Bet
                         print(f"you Loose! you now have:{Chips} chips\n")
                 elif Choice2 == "2":
                     if role in Red:
-                        Text("you win\n")
                         Chips = Chips + Bet
-                        print(f"you win! you now have:{Chips} chips\n") 
-                    else: 
+                        print(f"you win! you now have:{Chips} chips\n")
+                    else:
                         Chips = Chips - Bet
                         print(f"you Loose! you now have:{Chips} chips\n")
                 elif Choice2 == "3":
                     if role in High:
-                        Text("you win\n")
                         Chips = Chips + Bet
-                        print(f"you win! you now have:{Chips} chips\n") 
-                    else: 
+                        print(f"you win! you now have:{Chips} chips\n")
+                    else:
                         Chips = Chips - Bet
                         print(f"you Loose! you now have:{Chips} chips\n")
                 elif Choice2 == "4":
                     if role in Low:
-                        Text("you win\n")
                         Chips = Chips + Bet
-                        print(f"you win! you now have:{Chips} chips\n") 
-                    else: 
+                        print(f"you win! you now have:{Chips} chips\n")
+                    else:
                         Chips = Chips - Bet
                         print(f"you Loose! you now have:{Chips} chips\n")
-                        
+
                 elif Choice2 == "5":
                     if role in Even:
-                        Text("you win\n")
                         Chips = Chips + Bet
-                        print(f"you win! you now have:{Chips} chips\n") 
-                    else: 
+                        print(f"you win! you now have:{Chips} chips\n")
+                    else:
                         Chips = Chips - Bet
                         print(f"you Loose! you now have:{Chips} chips\n")
                 elif Choice2 == "6":
                     if role in Odd:
-                        Text("you win\n")
                         Chips = Chips + Bet
-                        print(f"you win! you now have:{Chips} chips\n") 
-                    else: 
+                        print(f"you win! you now have:{Chips} chips\n")
+                    else:
                         Chips = Chips - Bet
                         print(f"you Loose! you now have:{Chips} chips\n")
                 else:
-                    print("sohmeit is wrong")
+                    print("ERROR")
 
         elif Choice1 == "2":
-           AddChips = int(input("how much do you want to add: "))
-           Chips = Chips + AddChips
+            AddChips = int(input("\nhow much do you want to add: "))
+            Chips = Chips + AddChips
         elif Choice1 == "3":
-            break
+            Shutdown()
         else:
             print("invalid input")
+
+
 #I have output now I need to add betting; high low, red or black, even or odd, Straight/single(any number" 0 and 0. I am not doing the other types of bets damn it is 5:00 am
 def test(wheel_out):
     print(wheel_out)
@@ -117,6 +152,8 @@ def test(wheel_out):
         print("even")
     if wheel_out in Odd:
         print("odd")
+
+
 #test(rouletteweel())
 
-main(Chips)
+StartUp()
